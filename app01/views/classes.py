beca04bae1838266d9bd00de0a9b7febc7d1e1d9
auto007 <puk668@foxmail.com>
2018-09-22 01:03:09 +0800

    为班级分配老师

--- app01/views/classes.py
+++ app01/views/classes.py
@@ -3,6 +3,8 @@ from app01 import models
 
 def get_classes(request):
     cls_list = models.Classes.objects.all()
+    for item in cls_list:
+        print(item.id,item.title,item.m.all())
     return render(request,'get_classes.html',{'cls_list':cls_list})
 
 
@@ -28,4 +30,32 @@ def edit_classes(request):
         nid = request.GET.get('nid')
         title = request.POST.get('xxoo')
         models.Classes.objects.filter(id=nid).update(title=title)
+        return redirect('/classes.html')
+
+
+def set_teacher(request):
+    if request.method == 'GET':
+        nid = request.GET.get('nid')
+        cls_obj = models.Classes.objects.filter(id=nid).first()
+        #cls_teacher_list = cls_obj.m.all() #当前班级所有的老师对象
+        cls_teacher_list = cls_obj.m.all().values_list('id','name')
+
+        #当前班级任课老师的id列表
+        id_list = list(zip(*cls_teacher_list))[0] if list(zip(*cls_teacher_list)) else []
+
+        all_teacher_list = models.Teachers.objects.all()
+        return render(request,
+                      'set_teacher.html',
+                      {
+                          'id_list':id_list,
+                          'all_teacher_list':all_teacher_list,
+                          'nid':nid
+                      }
+                      )
+    elif request.method == 'POST':
+        nid = request.GET.get('nid')  #当前班级的id
+        ids = request.POST.getlist('teacher_ids')  #分配的老师的id
+        print('当前班级的id',nid, '分配的老师的id',ids)
+        obj = models.Classes.objects.filter(id=nid).first()
+        obj.m.set(ids)
         return redirect('/classes.html')
